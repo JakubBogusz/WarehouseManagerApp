90caa1c25b621f3938bbf08c4e92cf1d94d07d5a
Jakub Bogusz <jakub.bogusz@aptiv.com>
2021-05-17 22:20:14 +0200

    Creating UserModel and connecting with Api, creating spUserLookup store procedure, debugging SQL server

---
+++ RetailManagerData/dbo/StoredProcedures/spUserLookup.sql
@@ -0,0 +1,12 @@
+﻿CREATE PROCEDURE [dbo].[spUserLookup]
+	@Id nvarchar(128) 
+AS
+BEGIN
+
+	set nocount on;
+
+	SELECT Id, FirstName, LastName, EmailAddress, CreatedDate
+	FROM [dbo].[User]
+	WHERE Id = @Id
+
+END
---
+++ TRMDataManager/Controllers/UserController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using TRMDataManager.Library.DataAccess;
+using TRMDataManager.Library.Models;
+
+
+namespace TRMDataManager.Controllers
+{
+    [System.Web.Http.Authorize]
+    public class UserController : ApiController
+    {
+      
+        public List<UserModel> GetById()
+        {
+            string userId = RequestContext.Principal.Identity.GetUserId();
+            UserData data = new UserData();
+
+            return data.GetUserById(userId);
+
+        }
+
+      
+    }
+}
