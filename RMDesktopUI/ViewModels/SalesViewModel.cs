882617a5a77a216ab6bff3a2666b8f58e1cc7f15
Jakub Bogusz <jakub.bogusz@aptiv.com>
2021-05-23 22:08:39 +0200

    Create the Sales View part 2. Write the ViewModel dependecies.

--- RMDesktopUI/ViewModels/SalesViewModel.cs
+++ RMDesktopUI/ViewModels/SalesViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,7 +10,120 @@ namespace RMDesktopUI.ViewModels
 {
     public class SalesViewModel : Screen
     {
+        private BindingList<string> _products;
 
+        public BindingList<string> Products
+        {
+            get { return _products; }
+            set
+            {
+                _products = value; 
+                NotifyOfPropertyChange(() => Products);
+            }
+        }
+
+        private BindingList<string> _cart;
+
+        public BindingList<string> Cart
+        {
+            get { return _cart; }
+            set
+            {
+                _cart = value;
+                NotifyOfPropertyChange(() => Cart);
+            }
+        }
+
+        private string _itemQuantity;
+
+        public string ItemQuantity
+        {
+            get { return _itemQuantity; }
+            set
+            {
+                _itemQuantity = value;
+                NotifyOfPropertyChange(() => Products);
+            }
+        }
+
+        public string SubTotal
+        {
+            get
+            {
+                // TODO - Replace with calculation
+                return "$0.00";
+            }
+          
+        }
+
+        public string Tax
+        {
+            get
+            {
+                // TODO - Replace with calculation
+                return "$0.00";
+            }
+
+        }
+
+        public string Total
+        {
+            get
+            {
+                // TODO - Replace with calculation
+                return "$0.00";
+            }
+
+        }
+
+        public bool CanAddToCart
+        {
+            get
+            {
+                bool output = false;
+
+                //Make sure something is selected and ife there is an item quantity
+                
+
+                return output;
+            }
+        }
+
+        public bool RemoveFromCart
+        {
+            get
+            {
+                bool output = false;
+
+                //Make sure something is selected 
+
+
+                return output;
+            }
+        }
+
+        public void AddToCart()
+        {
+            
+        }
+
+        public bool CanCheckOut
+        {
+            get
+            {
+                bool output = false;
+
+                //Make sure something is something in the cart
+
+
+                return output;
+            }
+        }
+
+        public void CheckOut()
+        {
+
+        }
 
     }
 }
