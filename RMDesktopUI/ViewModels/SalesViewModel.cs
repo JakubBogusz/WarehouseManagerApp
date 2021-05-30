504219fadd1d1034b39d77716449b9065f0ab915
Jakub Bogusz <jakub.bogusz@aptiv.com>
2021-05-30 21:31:18 +0200

    Get Adding to Cart working
    Add the functionalities to add items to the cart and calculate the subtotal value

--- RMDesktopUI/ViewModels/SalesViewModel.cs
+++ RMDesktopUI/ViewModels/SalesViewModel.cs
@@ -31,6 +31,20 @@ namespace RMDesktopUI.ViewModels
             Products = new BindingList<UIProductModel>(productList);
         }
 
+
+        private UIProductModel _selectedProduct;
+
+        public UIProductModel SelectedProduct
+        {
+            get { return _selectedProduct; }
+            set
+            {
+                _selectedProduct = value;
+                NotifyOfPropertyChange(() => SelectedProduct);
+                NotifyOfPropertyChange(() => CanAddToCart);
+            }
+        }
+
         private BindingList<UIProductModel> _products;
 
         public BindingList<UIProductModel> Products
@@ -43,9 +57,9 @@ namespace RMDesktopUI.ViewModels
             }
         }
 
-        private BindingList<string> _cart;
+        private BindingList<CartItemModel> _cart = new BindingList<CartItemModel>();
 
-        public BindingList<string> Cart
+        public BindingList<CartItemModel> Cart
         {
             get { return _cart; }
             set
@@ -55,7 +69,7 @@ namespace RMDesktopUI.ViewModels
             }
         }
 
-        private int _itemQuantity;
+        private int _itemQuantity = 1;
 
         public int ItemQuantity
         {
@@ -64,6 +78,7 @@ namespace RMDesktopUI.ViewModels
             {
                 _itemQuantity = value;
                 NotifyOfPropertyChange(() => ItemQuantity);
+                NotifyOfPropertyChange(() => CanAddToCart);
             }
         }
 
@@ -71,8 +86,14 @@ namespace RMDesktopUI.ViewModels
         {
             get
             {
-                // TODO - Replace with calculation
-                return "$0.00";
+                decimal subTotal = 0;
+
+                foreach (var item in Cart)
+                {
+                    subTotal += (item.Product.RetailPrice * item.QuantityInCart);
+                }
+
+                return subTotal.ToString("C");
             }
           
         }
@@ -99,18 +120,52 @@ namespace RMDesktopUI.ViewModels
 
         public bool CanAddToCart
         {
+            
             get
             {
                 bool output = false;
-
-                //Make sure something is selected and ife there is an item quantity
+                
+                if (ItemQuantity > 0 && SelectedProduct?.QuantityInStock >= ItemQuantity)
+                {
+                    output = true;
+                }
                 
 
                 return output;
             }
         }
+        public void AddToCart()
+        {
+            CartItemModel existingItem = Cart.FirstOrDefault(x => x.Product == SelectedProduct);
 
-        public bool RemoveFromCart
+            if (existingItem != null)
+            {
+                existingItem.QuantityInCart += ItemQuantity;
+                SelectedProduct.QuantityInStock -= ItemQuantity;
+
+                // Not the best way of refresh the value in the cart
+                Cart.Remove(existingItem);
+                Cart.Add(existingItem);
+            }
+            else
+            {
+                CartItemModel item = new CartItemModel
+                {
+                    Product = SelectedProduct,
+                    QuantityInCart = ItemQuantity
+                };
+
+                Cart.Add(item);
+            }
+
+            SelectedProduct.QuantityInStock -= ItemQuantity;
+            ItemQuantity = 1;
+            NotifyOfPropertyChange(() => SubTotal);
+            NotifyOfPropertyChange(() => existingItem.DisplayText);
+
+        }
+
+        public bool CanRemoveFromCart
         {
             get
             {
@@ -123,10 +178,11 @@ namespace RMDesktopUI.ViewModels
             }
         }
 
-        public void AddToCart()
+        public void RemoveFromCart()
         {
-            
+            NotifyOfPropertyChange(() => SubTotal);
         }
+     
 
         public bool CanCheckOut
         {
