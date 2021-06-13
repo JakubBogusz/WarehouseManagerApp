c5bddd277b5470b9b9e4b02aac95e947993ec154
Jakub Bogusz <jakub.bogusz@aptiv.com>
2021-06-13 17:17:08 +0200

    Create stored procedures for Inventory

---
+++ RetailManagerData/dbo/StoredProcedures/spInventory_GetAll.sql
@@ -0,0 +1,10 @@
+﻿CREATE PROCEDURE [dbo].[spInventory_GetAll]
+	@param1 int = 0,
+	@param2 int
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	SELECT [Id], [ProductId], [Quantity], [PurchasePrice], [PurchaseDate]
+	FROM dbo.Inventory
+END
