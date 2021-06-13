6ce61ebb239b177c60da03b46c96931e85d78af8
Jakub Bogusz <jakub.bogusz@aptiv.com>
2021-06-13 21:28:54 +0200

    Add User Endpoint to the API, start working on UserViewModel in the future

---
+++ RMDesktopUI.Library/Api/UserEndpoint.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using RMDesktopUI.Library.Models;
+
+namespace RMDesktopUI.Library.Api
+{
+    public class UserEndpoint : IUserEndpoint
+    {
+        private readonly IApiHelper _apiHelper;
+
+        public UserEndpoint(IApiHelper apiHelper)
+        {
+            _apiHelper = apiHelper;
+        }
+
+        public async Task<List<ApplicationUserModel>> GetAll()
+        {
+            using (HttpResponseMessage response = await _apiHelper.ApiClient.GetAsync("/api/User/Admin/GetAllUsers"))
+            {
+                if (response.IsSuccessStatusCode)
+                {
+                    var result = await response.Content.ReadAsAsync<List<ApplicationUserModel>>();
+
+                    return result;
+                }
+                else
+                {
+                    throw new Exception(response.ReasonPhrase);
+                }
+            }
+        }
+    }
+}
--- TRMDataManager/Controllers/UserController.cs
+++ TRMDataManager/Controllers/UserController.cs
@@ -4,16 +4,16 @@ using System.Linq;
 using System.Web;
 using System.Web.Http;
 using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
 using TRMDataManager.Library.DataAccess;
 using TRMDataManager.Library.Models;
-
+using TRMDataManager.Models;
 
 namespace TRMDataManager.Controllers
 {
     [Authorize]
     public class UserController : ApiController
     {
-      
         [HttpGet]
         public UserModel GetById()
         {
@@ -24,6 +24,39 @@ namespace TRMDataManager.Controllers
 
         }
 
-      
+        [Authorize(Roles = "Admin")]
+        [HttpGet]
+        [Route("Admin/GetAllUsers")]
+        public List<ApplicationUserModel> GetAllUsers()
+        {
+            List<ApplicationUserModel> output = new List<ApplicationUserModel>();
+
+            using (var context = new ApplicationDbContext())
+            {
+                var userStore = new UserStore<ApplicationUser>(context);
+                var userManager = new UserManager<ApplicationUser>(userStore);
+
+                var users = userManager.Users.ToList();
+                var roles = context.Roles.ToList();
+
+                foreach (var user in users)
+                {
+                    ApplicationUserModel userModel = new ApplicationUserModel
+                    {
+                        Id = user.Id,
+                        Email = user.Email
+                    };
+
+                    foreach (var r in user.Roles)
+                    {
+                        userModel.Roles.Add(r.RoleId, roles.Where(x => x.Id == r.RoleId).First().Name);
+                    }
+
+                    output.Add(userModel);
+                }
+            }
+
+            return output;
+        }
     }
 }
