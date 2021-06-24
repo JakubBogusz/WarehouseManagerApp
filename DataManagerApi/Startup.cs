163d0730a8b00ce9944dd2179d0ab7f9a39f3bcc
Jakub Bogusz <jakub.bogusz@aptiv.com>
2021-06-24 12:27:37 +0200

    Move Security Key to Configuration

--- DataManagerApi/Startup.cs
+++ DataManagerApi/Startup.cs
@@ -58,8 +58,7 @@ namespace DataManagerApi
                     jwtBearerOptions.TokenValidationParameters = new TokenValidationParameters
                     {
                         ValidateIssuerSigningKey = true,
-                        IssuerSigningKey =
-                            new SymmetricSecurityKey(Encoding.UTF8.GetBytes("MySecretKeyIsSecretSoDoNotTell")),
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration.GetValue<string>("Secrets:SecurityKey"))),
                         ValidateIssuer = false,
                         ValidateAudience = false,
                         ValidateLifetime = true,
