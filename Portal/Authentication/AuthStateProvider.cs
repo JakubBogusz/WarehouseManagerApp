1cfec6d03562256e8fcec42de7f132994cc9ca79
Jakub Bogusz <jakub.bogusz@aptiv.com>
2021-08-23 21:53:30 +0200

    Create Authentication State provider

---
+++ Portal/Authentication/AuthStateProvider.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.Authorization;
+
+namespace Portal.Authentication
+{
+    public class AuthStateProvider : AuthenticationStateProvider
+    {
+        private readonly HttpClient _httpClient;
+        private readonly ILocalStorageService _localStorageService;
+        private readonly AuthenticationState _anonymousState;
+
+        public AuthStateProvider(HttpClient httpClient, ILocalStorageService localStorageService)
+        {
+            _httpClient = httpClient;
+            _localStorageService = localStorageService;
+        }
+
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            var token = await _localStorageService.GetItemAsync<string>("authToken");
+
+            if (string.IsNullOrWhiteSpace(token))
+            {
+                return _anonymousState;
+            }
+
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("bearer", token);
+
+            return new AuthenticationState(
+                new ClaimsPrincipal(new 
+                    ClaimsIdentity(JwtParser.ParseClaimsFromJwt(token), "jwtAuthType")));
+        }
+
+        public void NotifyUserAuthentication(string token)
+        {
+            var authenticatedUser = new ClaimsPrincipal(new
+                    ClaimsIdentity(JwtParser.ParseClaimsFromJwt(token), "jwtAuthType"));
+
+            var authState = Task.FromResult(new AuthenticationState(authenticatedUser));
+            NotifyAuthenticationStateChanged(authState);
+        }
+
+        public void NotifyUserLogout()
+        {
+            var authState = Task.FromResult(_anonymousState);
+            NotifyAuthenticationStateChanged(authState);
+        }
+    }
+}
